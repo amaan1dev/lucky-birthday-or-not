ee38b1b48aed7dd8cae45bfc23434e2038da3c4d
amaan1dev <amaan.lko32@gmail.com>
2022-09-15 04:35:41 +0530

    app ready

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 const birthDate = document.querySelector("#birth-date");
 const luckyNumber = document.querySelector("#lucky-number");
 
-console.log(birthDate, luckyNumber);
+const checkButton = document.querySelector("#check-button");
+
+const outputPara = document.querySelector("#output-para");
+
+function compareInputs(sum, luckyNumber)
+{
+    if (sum % luckyNumber === 0){
+    outputPara.innerText = "hurray your birthday is lucky 🚀";
+} else{
+    outputPara.innerText = "your birthday is not so lucky 🤔" ;
+}
+    
+} 
+
+function checkBirthdateIsLucky(){
+    const dob = birthDate.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob)
+    compareInputs(sum,luckyNumber.value);
+    else{
+        outputPara.innerText = "kindly enter both the details 🕵️‍♂️"
+    }
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(let i = 0; i < dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+ 
+
+
+checkButton.addEventListener('click', checkBirthdateIsLucky);
+    
